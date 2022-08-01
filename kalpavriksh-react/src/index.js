62c901122a819cda45d077ad771698085e6e80af
Sai Sashreek D <sai@5thdt.com>
2022-08-01 11:56:33 +0530

    Initial Commit

---
+++ kalpavriksh-react/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+const rootelment = document.getElementById("root");
+const root = createRoot(rootelment);
+root.render(<App />);
