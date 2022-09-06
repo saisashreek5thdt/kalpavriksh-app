a64dd50827ea6a6fd9a6d5e08c9f2fb4b46e64f9
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2022-09-06 09:24:06 +0530

    Appjs Updated

--- kalpavriksh-react/src/App.js
+++ kalpavriksh-react/src/App.js
@@ -6,24 +6,6 @@ import {
   Redirect,
 } from "react-router-dom";
 
-<<<<<<< HEAD
-import 'tw-elements';
-
-import Login from "./user/Login";
-import UserDashboard from "./user/UserDashboard";
-import CreatePatient from "./user/CreatePatient";
-import PatientDashboard from "./user/patient/PatientDashboard";
-
-import { DoctorDashboard } from "./pages";
-
-import { AuthContext } from "./context/auth-context";
-import CreateForm from "./user/CreateForm";
-import UploadDietchart from "./user/UploadDietchart";
-
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  //const [userId, setUserId] = useState(false);
-=======
 import { AuthContext } from "./context/auth-context";
 
 import Login from "./user/Login";
@@ -44,7 +26,6 @@ import Stepper from "./pages/form-validations/Stepper";
 const App = () => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
->>>>>>> c2b858116a3c474af3fab495f0863220a17eaee0
 
   const login = useCallback(() => {
     setIsLoggedIn(true);
@@ -58,48 +39,6 @@ const App = () => {
 
   if (!isLoggedIn) {
     routes = (
-<<<<<<< HEAD
-      <Switch>
-        <Route path="/" exact>
-          {/* <UserDashboard /> */}
-          <Login />
-        </Route>
-        <Route path="/userrole/" exact>
-          {/* <Login /> */}
-          <UserDashboard />
-        </Route>
-        <Route path="/userrole/createPatient" exact>
-          <CreatePatient />
-          {/* <UserDashboard /> */}
-        </Route>
-        <Route path="/userrole/createForm" exact>
-          <CreateForm />
-          {/* <UserDashboard /> */}
-        </Route>
-        <Route path="/userrole/uploadDietChart" exact>
-          <UploadDietchart />
-          {/* <UserDashboard /> */}
-        </Route>
-        <Route path="/userrole/patient" exact>
-          <PatientDashboard />
-        </Route>
-        <Route path="/doctordashboard" exact>
-          <DoctorDashboard />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    );
-  } else {
-    routes = (
-      <Switch>
-        <Route path="/" exact>
-          {/* <UserDashboard /> */}
-          <Login />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    );
-=======
       <Router>
         <Switch>
           <Route path="/" exact>
@@ -145,7 +84,6 @@ const App = () => {
           <Redirect to="/" />
         </Switch>
       </Router>
->>>>>>> c2b858116a3c474af3fab495f0863220a17eaee0
   }
 
   return (
