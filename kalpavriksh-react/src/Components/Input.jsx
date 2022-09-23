1968775e23f42560cefb1f004eef77c6fafd97ad
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2022-09-23 11:37:46 +0530

    Merge Conflict Issues

--- kalpavriksh-react/src/Components/Input.jsx
+++ kalpavriksh-react/src/Components/Input.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import react from 'react'
-
-const Input = ({label, input, name, id }) => {
-    return (
-        <div className="col-span-6 sm:col-span-3">
-            <label htmlFor="phone-number"
-                className="block text-sm font-medium text-gray-700">
-                {label}
-            </label>
-            <input type={input}
-                name={name}
-                id={id}
-                autoComplete={true}
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-        </div>
-    )
-}
-
-export default Input
-=======
 import React, {useReducer, useEffect} from "react";
 
 import { validate } from "../util/validators";
@@ -103,4 +82,3 @@ const Input = (props) => {
 };
 
 export default Input;
->>>>>>> c2b858116a3c474af3fab495f0863220a17eaee0
--- kalpavriksh-react/tailwind.config.js
+++ kalpavriksh-react/tailwind.config.js
@@ -1,15 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
-<<<<<<< HEAD
     "./src/**/*.{js,jsx,ts,tsx}",
-=======
-    "./src/**/*.{js,jsx,ts,tsx}", 
->>>>>>> c2b858116a3c474af3fab495f0863220a17eaee0
-=======
-    "./src/**/*.{js,jsx,ts,tsx}",
->>>>>>> 686a2da6c3a61dacf0d642b2651b140f77dd5b8a
     './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
@@ -20,20 +12,14 @@ module.exports = {
     },
   },
   plugins: [
-<<<<<<< HEAD
     require('tw-elements/dist/plugin'),
     require('@tailwindcss/forms'),
-=======
     require('tailwindcss'),
     require('autoprefixer'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
-<<<<<<< HEAD
->>>>>>> c2b858116a3c474af3fab495f0863220a17eaee0
-=======
     require('tw-elements/dist/plugin')
->>>>>>> 686a2da6c3a61dacf0d642b2651b140f77dd5b8a
   ],
 }
