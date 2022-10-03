1282fcb53df3113eea65cb3b1a49a6bfa3cebae1
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2022-10-03 09:33:38 +0530

    Doctor Dashboard Tabs Updated

--- kalpavriksh-react/src/Components/TabNav.jsx
+++ kalpavriksh-react/src/Components/TabNav.jsx
@@ -9,48 +9,7 @@ const TabNav = () => {
         id="tabs-tabJustify"
         role="tablist"
       >
-        <li className="nav-item flex-grow text-center" role="presentation">
-          <a
-            href="#tabs-homeJustify"
-            className="nav-link w-full block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent active"
-            id="tabs-home-tabJustify"
-            data-bs-toggle="pill"
-            data-bs-target="#tabs-homeJustify"
-            role="tab"
-            aria-controls="tabs-homeJustify"
-            aria-selected="true"
-          >
-            Enroll Patient
-          </a>
-        </li>
-        <li className="nav-item flex-grow text-center" role="presentation">
-          <a
-            href="#tabs-profileJustify"
-            className=" nav-link w-full block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent "
-            id="tabs-profile-tabJustify"
-            data-bs-toggle="pill"
-            data-bs-target="#tabs-profileJustify"
-            role="tab"
-            aria-controls="tabs-profileJustify"
-            aria-selected="false"
-          >
-            Create Form
-          </a>
-        </li>
-        <li className="nav-item flex-grow text-center" role="presentation">
-          <a
-            href="#tabs-messagesJustify"
-            className="nav-link w-full block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
-            id="tabs-messages-tabJustify"
-            data-bs-toggle="pill"
-            data-bs-target="#tabs-messagesJustify"
-            role="tab"
-            aria-controls="tabs-messagesJustify"
-            aria-selected="false"
-          >
-            Upload DietChart
-          </a>
-        </li>
+        
         <li className="nav-item flex-grow text-center" role="presentation">
           <a
             href="#tabs-appointmentsJustify"
