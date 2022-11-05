76f0fef5beab2734401051f83f386bb06393307d
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2022-11-05 09:49:41 +0530

    New Codes are Pushed in Backend with Schema

--- kalpavriksh-node/config/db.js
+++
@@ -1,7 +0,0 @@
-const { MongoClient } = require("mongodb");
-//Conncection URL
-const url ="mongodb+srv://kalpavriksh:healthapp@kalpavriksh.rvtrlvy.mongodb.net/kalpavrikshapp?retryWrites=true&w=majority";
-exports.client = new MongoClient(url);
-
-//Database Name
-exports.dbName = "kalpavriksh-app";
