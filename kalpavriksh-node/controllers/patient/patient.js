cb1c48b766a6b1a7bd294ae17caae4ea6bcafc0c
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2022-11-15 10:15:16 +0530

    New Codes Pushed on Backend

--- kalpavriksh-node/controllers/patient/patient.js
+++ kalpavriksh-node/controllers/patient/patient.js
@@ -30,3 +30,51 @@ module.exports.addPatient = async (req, res) => {
     }
 };
 
+module.exports.getAllPatients = async (req, res) => {
+    try {
+        const patients = await Patient.find({doctorId: "dinesh"});
+        return res.status(200).json({
+            success: true,
+            message: "Successfully got all patients",
+            data: patients
+        })
+    } catch(err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+};
+
+module.exports.getPatient = async (req, res) => {
+    try {
+        const patient = await Patient.findOne({doctorId: "dinesh", _id: req.params.id});
+        return res.status(200).json({
+            success: true,
+            message: "Successfully got the patient",
+            data: patient
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+};
+
+module.exports.editPatient = async (req, res) => {
+    try {
+        await Patient.findOneAndUpdate({doctorId: "dinesh", _id: req.params.id}, req.body);
+
+        return res.status(200).json({
+            success: true,
+            message: "Successfully updated the patient",
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+};
+
--- kalpavriksh-node/routes/doctor.js
+++ kalpavriksh-node/routes/doctor.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 
-const { addPatient } = require('../controllers/patient/patient');
+const { addPatient, getAllPatients, getPatient, editPatient } = require('../controllers/patient/patient');
+const dietChart = require('../controllers/diet-chart');
 
 router.post('/add-patient', addPatient);
+router.get('/get-all-patients', getAllPatients);
+router.get('/patient/:id', getPatient);
+router.put('/edit-patient/:id', editPatient);
+router.post('/add-diet-chart', dietChart.addDietChart);
 
 module.exports = router;
